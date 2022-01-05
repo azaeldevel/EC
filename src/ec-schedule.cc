cfb1c4a52f9e1d27f392ec595985503bd393961f
Azael <azael.devel@gmail.com>
2022-01-05 15:49:18 -0800

    saving..

--- src/ec-schedule.cc
+++ src/ec-schedule.cc
@@ -2,45 +2,52 @@
 /*
  * main.cc
  * Copyright (C) 2021 Azael Reyes <azael.devel@gmail.com>
- * 
+ *
  * AE is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * AE is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 
 #include <fstream>
-#include <octetos/core/shell.hh>
 #include <string>
+
+#if defined(__GNUC__) && defined(__linux__)
+    #include <octetos/core/shell.hh>
+#elif defined(__GNUC__) && (defined(_WIN32) || defined(_WIN64))
+    #include <shell.hh>
+#else
+    #error "Pltaforma desconocida"
+#endif
 
 #include "schedule.hh"
 
 int main(int argc, const char* argv[])
 {
-	/*if(argc < 2) 
+	/*if(argc < 2)
 	{
 		std::cerr << "Indique el parametro necesario\n";
 		std::cerr << "trans  dirlog\n";
 		return EXIT_SUCCESS;
 	}*/
-	
+
 	oct::ec::sche::Enviroment* sche = new oct::ec::sche::Enviroment("logs","tests");
 	sche->enableEcho(&std::cout,2);
-	
+
 	bool ret;
 	ret = sche->run();
-	
+
 	delete sche;
-	
+
 	return ret? EXIT_SUCCESS : EXIT_FAILURE;
 }
 
