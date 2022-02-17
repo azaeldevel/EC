4de168e979ece0ab8fd9f349f96bd34e31dd11aa
Azael <azael.devel@gmail.com>
2022-02-16 20:13:22 -0800

    save

--- src/Program-tree.hh
+++ src/Program-tree.hh
@@ -9,50 +9,88 @@
 #include "ec.hh"
 
 
-namespace oct::ec::prog
+namespace oct::ec::prog::ops
 {
-
-    template<typename T> class Plus : public std::vector<T>
+    template<typename T> class Operation : public std::vector<T>
     {
     public:
-        Plus(const T& a, const T& b) : std::vector<T>(2)
+        Operation(unsigned int ops) : std::vector<T>(ops)
+        {
+        }
+
+        virtual operator T() const = 0;
+    };
+
+    template<typename T> class Plus : public Operation<T>
+    {
+    public:
+        Plus(const T& a, const T& b) : Operation<T>(2)
         {
             std::vector<T>::at(0) = a;
             std::vector<T>::at(1) = b;
         }
 
-        operator T()
+        virtual operator T() const
         {
             return std::vector<T>::front() + std::vector<T>::back();
         }
     };
-    template<typename T> class Rest : public std::vector<T>
+    template<typename T> class Rest : public Operation<T>
     {
     public:
-        Rest(const T& a, const T& b) : std::vector<T>(2)
+        Rest(const T& a, const T& b) : Operation<T>(2)
         {
             std::vector<T>::at(0) = a;
             std::vector<T>::at(1) = b;
         }
 
-        operator T()
+        virtual operator T() const
         {
             return std::vector<T>::front() - std::vector<T>::back();
         }
     };
 
-    template<typename T> class Expresion : public std::list<std::vector<T>>
+}
+namespace oct::ec::prog::expr
+{
+    template<typename T> class Operation : public std::vector<ops::Operation<T>*>
     {
     public:
-
-        operator T()
+        Operation(unsigned int ops) : std::vector<ops::Operation<T>*>(ops)
         {
+        }
 
+        virtual operator T() const = 0;
+    };
+
+    template<typename T> class Plus : public Operation<T>
+    {
+    public:
+        Plus(ops::Operation<T>& a, ops::Operation<T>& b) : Operation<T>(2)
+        {
+            std::vector<ops::Operation<T>*>::at(0) = &a;
+            std::vector<ops::Operation<T>*>::at(1) = &b;
+        }
+
+        virtual operator T() const
+        {
+            return *std::vector<ops::Operation<T>*>::front() + *std::vector<ops::Operation<T>*>::back();
+        }
+    };
+    template<typename T> class Rest : public Operation<T>
+    {
+    public:
+        Rest(ops::Operation<T>& a, ops::Operation<T>& b) : Operation<T>(2)
+        {
+            std::vector<ops::Operation<T>*>::at(0) = &a;
+            std::vector<ops::Operation<T>*>::at(1) = &b;
+        }
+
+        virtual operator T() const
+        {
+            return *std::vector<ops::Operation<T>*>::front() - *std::vector<ops::Operation<T>*>::back();
         }
     };
 
-
-
 }
-
 #endif
--- src/main.cc
+++ src/main.cc
@@ -26,11 +26,20 @@ using namespace oct::ec::prog;
 int main(int argc, const char* argv[])
 {
 
-    Plus plus1(10,25);
-    Rest rest1(11,9);
+    ops::Plus plus1(10,25);
+    ops::Rest rest1(11,9);
+    ops::Plus plus2(3,9);
+    ops::Rest rest2(15,9);
 
-    std::cout << "Suma : " << plus1 << std::endl;
-    std::cout << "Rest : " << rest1 << std::endl;
+    std::cout << "Suma 1: " << plus1 << std::endl;
+    std::cout << "Rest 1: " << rest1 << std::endl;
+    std::cout << "Suma 2: " << plus2 << std::endl;
+    std::cout << "Rest 2: " << rest2 << std::endl;
+
+    expr::Plus expre_plus1(rest1,rest2);
+    std::cout << "Expresion 1: " << expre_plus1 << std::endl;
+    expr::Rest expre_rest1(rest1,rest2);
+    std::cout << "Expresion 2: " << expre_rest1 << std::endl;
 
 	return EXIT_SUCCESS ;
 }
