4a4db59b1e50b0fa22107adfcde206549217f298
Azael Reyes <azael.devel@gmail.com>
2022-02-20 13:14:17 -0800

    save

--- src/ec.cc
+++ src/ec.cc
@@ -752,7 +752,7 @@ bool Enviroment::run()
 		leader = front();
 		//std::cout << "\tEnviroment::run - while Step 2\n";
 
-		media = oct::sta::mean<ec::Single,real>(*this,[](const ec::Single* s)->real{return s->getFitness();});
+		media = oct::sta::mean<ec::Single*,real>(*this,[](ec::Single* s)->real{return s->getFitness();});
 		for(ec::Single* s : *this)
 		{
 			//std::cout << "\t" << s->getID() << " Adaptabilidad : " << s->getFitness() << "\n";
@@ -773,7 +773,7 @@ bool Enviroment::run()
 			sigma += pow(s->getFitness() - media,2);
 		}
 		sigma /= real(size());*/
-		sigma = oct::sta::variation<ec::Single,real>(*this,[](const ec::Single* s)->real{return s->getFitness();});
+		sigma = oct::sta::variation<ec::Single*,real>(*this,[](ec::Single* s)->real{return s->getFitness();});
 		/*if(leaderPrev != leader)
 		{//hay un nuevo lider
 			//TODO:optener una promedio a partir de una progresion
